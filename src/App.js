c1e9d169e1e1f0abc5449b5835af4d41d32c6f8f
masturdating1991 <aligodosi@gmail.com>
2021-01-18 19:56:29 +0330

    init

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect, useCallback } from 'react'
+import { FaSearch } from 'react-icons/fa'
+import Photo from './Photo'
+
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const mainUrl = `https://api.unsplash.com/photos/`
+const searchUrl = `https://api.unsplash.com/search/photos/`
+
+function App() {
+    const [loading, setLoading] = useState(false)
+    const [photos, setPhotos] = useState([])
+    const [page, setPage] = useState(0)
+    const [query, setQuery] = useState('')
+
+
+    const fetchImages = useCallback(async () => {
+        setLoading(true)
+        let url
+        const urlPage = `&page=${page}`
+        const urlQuery = `&query=${query}`
+
+        if (query) {
+            url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
+        } else {
+            url = `${mainUrl}${clientID}${urlPage}`
+        }
+
+        try {
+            const res = await fetch(url)
+            const data = await res.json()
+
+            setPhotos((oldPhotos) => {
+                if (query && page === 1) {
+                    return data.results
+                } else if (query) {
+                    return [...oldPhotos, ...data.results]
+                } else {
+                    return [...oldPhotos, ...data]
+                }
+            })
+            setLoading(false)
+        } catch (err) {
+            setLoading(false)
+            console.log(err)
+        }
+    }, [page, query])
+
+    useEffect(() => {
+        fetchImages()
+    }, [page, fetchImages])
+
+    useEffect(() => {
+        const event = window.addEventListener('scroll', () => {
+            if (!loading && (window.innerHeight + window.scrollY) >= document.body.scrollHeight - 3) {
+                setPage(oldPage => {
+                    return oldPage + 1
+                })
+            }
+        })
+        return () => window.removeEventListener('scroll', event)
+        // eslint-disable-next-line
+    }, [])
+
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        setPage(1)
+    }
+
+    return (
+        <main>
+            <section className="search">
+                <form className="search-form">
+                    <input
+                        type="text"
+                        placeholder="search"
+                        className="form-input"
+                        value={query}
+                        onChange={e => setQuery(e.target.value)}
+                    />
+                    <button type="submit" className="submit-btn" onClick={handleSubmit}>
+                        <FaSearch style={{ cursor: "pointer" }} />
+                    </button>
+                </form>
+            </section>
+
+            <section className="photos">
+                <div className="photos-center">
+                    {
+                        photos.map((item, index) =>
+                            <Photo key={index} {...item} />
+                        )
+                    }
+                </div>
+                {
+                    loading && <h2 className="loading">Loading...</h2>
+                }
+            </section>
+        </main>
+    )
+}
+
+export default App
